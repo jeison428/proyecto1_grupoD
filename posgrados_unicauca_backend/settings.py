3915fc14787bebe0afe0ce048dfab40329689769
Jeison Ortiz <jesonog@gmail.com>
2020-10-26 10:45:11 -0500

    Commit para actualizar y dejar el comentario el tipo de base de datos que se va a usar (MySQL - SQLite)

--- posgrados_unicauca_backend/settings.py
+++ posgrados_unicauca_backend/settings.py
@@ -96,14 +96,18 @@ WSGI_APPLICATION = 'posgrados_unicauca_backend.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'posgrados_unicauca_backend_v2',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': 3306,
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'posgrados_unicauca_backend_v2',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': 3306,
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 }
 
 
