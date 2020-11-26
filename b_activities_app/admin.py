48159b9d50cfb95c98e45015baa88f3def48375b
Jeison Ortiz <jesonog@gmail.com>
2020-11-26 10:45:04 -0500

    se actualizo el archivo admin.py del grupo b con los nombres de los nuevos modelos

--- b_activities_app/admin.py
+++ b_activities_app/admin.py
@@ -1,35 +1,13 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-
-'''
-class ponenciaCongresoAdmin(admin.ModelAdmin):
-    readonly_fields=('created','updated')
-
-class publicacionAdmin(admin.ModelAdmin):
-    readonly_fields=('created','updated')
-
-class cursoAdmin(admin.ModelAdmin):
-    readonly_fields=('created','updated')
-
-class exposicionResultadosAdmin(admin.ModelAdmin):
-    readonly_fields=('created','updated')
-
-class estanciaInvestigacionAdmin(admin.ModelAdmin):
-    readonly_fields=('created','updated')
-
-class parcipacionProyectoAdmin(admin.ModelAdmin):
-    readonly_fields=('created','updated')
-
-'''
-
-
-
-
-admin.site.register(ponenciaCongreso)
-admin.site.register(publicacion)
-admin.site.register(curso)
-admin.site.register(exposicionResultados)
-admin.site.register(estanciaInvestigacion)
-admin.site.register(parcipacionProyecto)
+admin.site.register([
+    Activity,
+    Lecture,
+    Publication,
+    ProjectCourse,
+    ResearchStays,
+    PresentationResults,
+    ParticipationProjects,
+    Prize
+])
