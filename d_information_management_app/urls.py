a52b337b2cfd33b5259848264482fc9ad5f89370
Jeison Ortiz <jesonog@gmail.com>
2020-11-28 20:16:09 -0500

    se agregaron las urls de las apis creadas antes 2

--- d_information_management_app/urls.py
+++ d_information_management_app/urls.py
@@ -64,6 +64,6 @@ urlpatterns = [
     path('api/1.0/consultar_trabaja/<int:id_gi>/<int:id_ac>', ConsultWorksInvestGroupAPI.as_view()),
     path('api/1.0/consultar_dirige/<int:id_p>/<int:id_gi>', ConsultManageInvestGroupAPI.as_view()),
     path('api/1.0/consultar_profesor/<int:id>', ConsultProfessor_idAPI.as_view()),
-    path('api/1.0/consultar_miembro_gi/<int:id>', ConsultMemberGIAPI.as_view()),
+    path('api/1.0/consultar_miembro_gi/<int:id>', ConsultMemberIGAPI.as_view()),
     path('api/1.0/consultar_miembro_p/<int:id>', ConsultMemberProfessorAPI.as_view()),
 ]
