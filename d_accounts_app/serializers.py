78ff6a5bf3b26b73cf6c073dd5f067bd8d0c7bda
Jeison Ortiz <jesonog@gmail.com>
2020-12-07 15:46:56 -0500

    se agrego el nuevo campo para verificar si un usuario es coordinador en el serializador que se usa para editar

--- d_accounts_app/serializers.py
+++ d_accounts_app/serializers.py
@@ -53,11 +53,13 @@ class UpdateUserSerializer(serializers.ModelSerializer):
     is_proffessor = serializers.BooleanField(required=False)
     is_student = serializers.BooleanField(required=False)
     is_active = serializers.BooleanField(required=False)
+    is_coordinator = serializers.BooleanField(required=False)
 
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'username', 'password', 'email', 'type_id', 'personal_id', 
-                'personal_code', 'photo', 'telephone', 'address', 'is_proffessor', 'is_student', 'is_active']
+                'personal_code', 'photo', 'telephone', 'address', 'is_proffessor', 'is_student', 'is_active', 
+                'is_coordinator']
 
 
 # class UserSerializer2(serializers.Serializer):
